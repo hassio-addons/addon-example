8f8a74300343b4db31dce817b1b4c2710bc53b67
Franck Nijhof <frenck@geekchimp.com>
2017-10-19 09:23:34 +0200

    :pencil: Tweaks code comments

--- example/rootfs/usr/bin/run.sh
+++ example/rootfs/usr/bin/run.sh
@@ -4,7 +4,7 @@
 # Community Hass.io Add-ons: Example
 #
 # Example add-on for Hass.io.
-# This add-on displays a random quote every 5 seconds.
+# This add-on displays a random quote every X seconds.
 #
 # ==============================================================================
 set -o errexit  # Exit script when a command exits with non-zero status
