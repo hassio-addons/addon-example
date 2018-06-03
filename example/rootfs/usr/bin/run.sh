23a7bbbb4e50fa5528d1a12603685a2f28429e6e
Franck Nijhof <frenck@geekchimp.com>
2018-06-03 12:29:44 +0200

    :shirt: Fixes shellcheck warnings

--- example/rootfs/usr/bin/run.sh
+++ example/rootfs/usr/bin/run.sh
@@ -34,11 +34,11 @@ get_quote_online() {
     html=$(wget -q -O - "http://www.quotationspage.com/quote/${number}.html")
 
     quote=$(grep -e "<dt>" -e "</dd>" <<< "${html}" \
-        | awk -F'[<>]' '{ 
+        | awk -F'[<>]' '{
             if($2 ~ /dt/)
-                { print $3 } 
+                { print $3 }
             else if($4 ~ /b/)
-                { print "-- " $7 "  n(" $19 ")"} 
+                { print "-- " $7 "  n(" $19 ")"}
         }'
     )
 
@@ -59,17 +59,17 @@ get_quote_offline() {
 
     hass.log.trace "${FUNCNAME[0]}"
 
-    quotes+=("Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.\n -Samuel Beckett")
-    quotes+=("Never give up, for that is just the place and time that the tide will turn.\n -Harriet Beecher Stowe")
-    quotes+=("Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.\n -Thomas A. Edison")
-    quotes+=("Life isn't about getting and having, it's about giving and being.\n -Kevin Kruse")
-    quotes+=("Strive not to be a success, but rather to be of value.\n -Albert Einstein")
-    quotes+=("You miss 100% of the shots you don't take.\n -Wayne Gretzky")
-    quotes+=("People who are unable to motivate themselves must be content with mediocrity, no matter how impressive their other talents. \n -Andrew Carnegie")
-    quotes+=("Design is not just what it looks like and feels like. Design is how it works.\n -Steve Jobs")
-    quotes+=("Only those who dare to fail greatly can ever achieve greatly.\n -Robert F. Kennedy")
-    quotes+=("All our dreams can come true, if we have the courage to pursue them.\n -Walt Disney")
-    quotes+=("Success consists of going from failure to failure without loss of enthusiasm.\n -Winston Churchill")
+    quotes+=("Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.\\n -Samuel Beckett")
+    quotes+=("Never give up, for that is just the place and time that the tide will turn.\\n -Harriet Beecher Stowe")
+    quotes+=("Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.\\n -Thomas A. Edison")
+    quotes+=("Life isn't about getting and having, it's about giving and being.\\n -Kevin Kruse")
+    quotes+=("Strive not to be a success, but rather to be of value.\\n -Albert Einstein")
+    quotes+=("You miss 100% of the shots you don't take.\\n -Wayne Gretzky")
+    quotes+=("People who are unable to motivate themselves must be content with mediocrity, no matter how impressive their other talents. \\n -Andrew Carnegie")
+    quotes+=("Design is not just what it looks like and feels like. Design is how it works.\\n -Steve Jobs")
+    quotes+=("Only those who dare to fail greatly can ever achieve greatly.\\n -Robert F. Kennedy")
+    quotes+=("All our dreams can come true, if we have the courage to pursue them.\\n -Walt Disney")
+    quotes+=("Success consists of going from failure to failure without loss of enthusiasm.\\n -Winston Churchill")
 
     number=$(( ( RANDOM % 11 )  + 1 ))
     echo "${quotes[$number]}"
@@ -95,7 +95,7 @@ display_quote() {
         hass.log.notice \
             'Could not connect to quotationspage.com, using an offline quote'
         quote=$(get_quote_offline)
-    fi 
+    fi
 
     quote=$(sed 's/n()//g' <<< "${quote}" | xargs -0 echo | fmt -40)
     timestamp=$(date +"%T")
