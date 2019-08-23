2c8733ade5be9239fa4c1709a87ea76410ba3270
Paul Sinclair <sinclairpaul@hotmail.com>
2019-08-23 18:53:07 -0400

    🔨Add multiple commands under S6

---
+++ example/rootfs/usr/bin/example2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+#
+# Community Hass.io Add-ons: Example
+#
+# Example add-on for Hass.io.
+#
+# ------------------------------------------------------------------------------
+main() {
+    local sleep
+
+    bashio::log.trace "${FUNCNAME[0]}"
+
+    while true; do
+        echo "Second Script Output"
+        sleep 10
+    done
+}
+main "$@"
